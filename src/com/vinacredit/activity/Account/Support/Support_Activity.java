4318e6b40e37eda7df31329a9fbff632e39f7d88
viethung232@gmail.com <viethung232@gmail.com>
2012-11-06 10:58:53 +0700

    -Hung modify Support_Activity

--- src/com/vinacredit/activity/Account/Support/Support_Activity.java
+++ src/com/vinacredit/activity/Account/Support/Support_Activity.java
@@ -1,5 +1,74 @@
 package com.vinacredit.activity.Account.Support;
 
-public class Support_Activity {
+import com.vinacredit.activity.R;
+import com.vinacredit.activity.Account.Account_Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Support_Activity extends Activity{
+
+	private Button btnAccount, btnVinacredit, btnHotline, btnInstruction;
+	private TextView txtTitleBar;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		initialize();
+	}
+	private void initialize() {
+		// TODO Auto-generated method stub
+		btnAccount		= (Button)findViewById(R.id.btnAccount);
+		btnVinacredit	= (Button)findViewById(R.id.btnVinacredit);
+		btnHotline		= (Button)findViewById(R.id.btnHotline);
+		btnInstruction	= (Button)findViewById(R.id.btnInstruction);
+		txtTitleBar		= (TextView)findViewById(R.id.txtTitleBar);
+		
+		//action btnAccount
+		btnAccount.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(),Account_Activity.class);
+				startActivity(intent);
+			}
+		});
+		
+		//action btnVinacredit
+		btnVinacredit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "Vinacredit", Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		//action btnHotline
+		btnHotline.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "Hotline", Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		//action btnInstruction
+		btnInstruction.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "Instruction", Toast.LENGTH_LONG).show();
+			}
+		});
+	}
 
 }
