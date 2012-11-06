c5b81a6a977ada87841bdc09cd5884b1b514c339
Subasa1985 <nguyen.luphuc@gmail.com>
2012-11-06 14:13:29 +0700

    Revert "-Hung modify Identify_Activity, Sending_Activity"
    This reverts commit 3453d918084c4bac427712c9c443ce728485be69.

--- src/com/vinacredit/activity/Sale/Sending/Sending_Activity.java
+++ src/com/vinacredit/activity/Sale/Sending/Sending_Activity.java
@@ -1,18 +1,5 @@
 package com.vinacredit.activity.Sale.Sending;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-
-public class Sending_Activity extends Activity{
-
-	private TextView	txtTitleBar;
-	private Button		btnback, btnReceipt;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-	}
+public class Sending_Activity {
 
 }
