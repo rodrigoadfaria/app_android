815a1adc0a5a164929108a7107aff16469cc713b
viethung232@gmail.com <viethung232@gmail.com>
2012-11-06 08:57:48 +0700

    +Hung add activity_done

--- src/com/vinacredit/activity/Account/Account_Activity.java
+++ src/com/vinacredit/activity/Account/Account_Activity.java
@@ -71,7 +71,7 @@ public class Account_Activity extends Activity{
 	}
 	private void initialize() {
 		// TODO Auto-generated method stub
-		imgUser			= (ImageView)findViewById(R.id.imgUser);
+		imgUser			= (ImageView)findViewById(R.id.imgUsername);
 		txtUsername 	= (TextView)findViewById(R.id.txtUsername);
 		txtEmail		= (TextView)findViewById(R.id.txtEmail);
 		btnSignOut		= (Button)findViewById(R.id.btnSignOut);
