fdc142b60e4f2fe9cc5be89f21e0e24bdac89e99
viethung232@gmail.com <viethung232@gmail.com>
2012-11-06 16:56:07 +0700

    -Hung modify Sending_Activity

--- src/com/vinacredit/activity/Sale/Sending/Sending_Activity.java
+++ src/com/vinacredit/activity/Sale/Sending/Sending_Activity.java
@@ -1,5 +1,27 @@
 package com.vinacredit.activity.Sale.Sending;
 
-public class Sending_Activity {
+import com.vinacredit.activity.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Sending_Activity extends Activity{
+
+	private Button		btnBack, btnReceipt;
+	private TextView	txtTitlebar;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sending);
+		initialize();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
