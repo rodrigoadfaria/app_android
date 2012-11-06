12c35e69dce0ab2204ba44fb517e0abcb844a23a
Subasa1985 <nguyen.luphuc@gmail.com>
2012-11-06 14:11:30 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "-Hung modify SaleHistory_Activity""""""""
    This reverts commit 0dd63278f83b86118efd6ebb670f9e6a125a040f.

--- src/com/vinacredit/activity/Account/SaleHistory/SaleHistory_Activity.java
+++ src/com/vinacredit/activity/Account/SaleHistory/SaleHistory_Activity.java
@@ -1,5 +1,46 @@
 package com.vinacredit.activity.Account.SaleHistory;
 
-public class SaleHistory_Activity {
+import com.vinacredit.activity.R;
+import com.vinacredit.activity.Account.Account_Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class SaleHistory_Activity extends Activity{
+
+	private Button		btnAccount;
+	private TextView	txtTitlteBar;
+	private ImageView	imgUsername;
+	private ListView	listView1;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		initialize();
+	}
+	private void initialize() {
+		// TODO Auto-generated method stub
+		btnAccount		= (Button)findViewById(R.id.btnAccount);
+		txtTitlteBar	= (TextView)findViewById(R.id.txtTitleBar);
+		imgUsername		= (ImageView)findViewById(R.id.imgUsername);
+		listView1		= (ListView)findViewById(R.id.listView1);
+		
+		//action btnAccount
+		btnAccount.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(),Account_Activity.class);
+				startActivity(intent);
+			}
+		});
+	}
 
 }
