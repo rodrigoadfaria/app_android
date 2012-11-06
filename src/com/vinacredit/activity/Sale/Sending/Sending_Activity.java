a36c13e536798f925fee85a8aa6adbafc077651a
viethung232@gmail.com <viethung232@gmail.com>
2012-11-06 17:24:09 +0700

    -Hung modify Sending_Activity

--- src/com/vinacredit/activity/Sale/Sending/Sending_Activity.java
+++ src/com/vinacredit/activity/Sale/Sending/Sending_Activity.java
@@ -1,9 +1,13 @@
 package com.vinacredit.activity.Sale.Sending;
 
 import com.vinacredit.activity.R;
+import com.vinacredit.activity.Sale.Receipt.Receipt_Activity;
+import com.vinacredit.activity.Sale.Signature.Signature_Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,7 +25,31 @@ public class Sending_Activity extends Activity{
 
 	private void initialize() {
 		// TODO Auto-generated method stub
+		btnBack		= (Button)findViewById(R.id.btnBack);
+		btnReceipt	= (Button)findViewById(R.id.btnReceipt);
+		txtTitlebar	= (Button)findViewById(R.id.txtTitleBar);
 		
+		//action btnBack
+		btnBack.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(),Signature_Activity.class);
+				startActivity(intent);
+			}
+		});
+		
+		//action btnReceipt
+		btnReceipt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(),Receipt_Activity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
