66f65a46a2ce5f5da1be4dc263de082b688149c3
viethung232@gmail.com <viethung232@gmail.com>
2012-11-07 11:40:53 +0700

    -Hung modify SignIn_Activity

--- src/com/vinacredit/activity/SignIn/SignIn_Activity.java
+++ src/com/vinacredit/activity/SignIn/SignIn_Activity.java
@@ -1,6 +1,6 @@
 package com.vinacredit.activity.SignIn;
 
-
+import com.vinacredit.Resource.*;
 import com.vinacredit.activity.R;
 import com.vinacredit.activity.InformationAccount.InformationAccount_Activity;
 import com.vinacredit.activity.SignIn.WrongPass.WrongPass_Activity;
@@ -11,11 +11,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignIn_Activity extends Activity{
 
-	private EditText edtUsername, edtPassword;
-	private Button btnSignIn, btnWrongPass;
+	private EditText 	edtUsername, edtPassword;
+	private Button 		btnSignIn, btnWrongPass;
+	private TextView	txtTitleBar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -25,10 +27,12 @@ public class SignIn_Activity extends Activity{
 	}
 	private void initialize() {
 		// TODO Auto-generated method stub
-		edtUsername = (EditText)findViewById(R.id.edtUsername);
-		edtPassword = (EditText)findViewById(R.id.edtPassword);
-		btnSignIn = (Button)findViewById(R.id.btnSignIn);
-		btnWrongPass = (Button)findViewById(R.id.btnWrongPass);
+		edtUsername 	= (EditText)findViewById(R.id.edtUsername);
+		edtPassword 	= (EditText)findViewById(R.id.edtPassword);
+		btnSignIn 		= (Button)findViewById(R.id.btnSignIn);
+		btnWrongPass 	= (Button)findViewById(R.id.btnWrongPass);
+		txtTitleBar		= (TextView)findViewById(R.id.txtTitleBar);
+		translate();
 		
 		btnSignIn.setOnClickListener(new View.OnClickListener() {
 			
@@ -49,6 +53,14 @@ public class SignIn_Activity extends Activity{
 				startActivity(i);
 			}
 		});
+	}
+	private void translate() {
+		// TODO Auto-generated method stub
+		edtUsername.setHint(MACROS.SIGNIN_EMAIL_TXT);
+		edtPassword.setHint(MACROS.SIGNIN_PASS_TXT);
+		btnSignIn.setText(MACROS.SIGNIN_BTN);
+		btnWrongPass.setText(MACROS.SIGNIN_FORGOT_BTN);
+		txtTitleBar.setText(MACROS.SIGNIN_LBL);
 	}
 
 }
