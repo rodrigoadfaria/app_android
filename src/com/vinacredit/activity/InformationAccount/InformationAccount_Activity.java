49addfe0cf6fb520e4c132ec9439ee993d8488f3
viethung232@gmail.com <viethung232@gmail.com>
2012-11-07 13:22:52 +0700

    -Hung modify InformationAccount_Activity

--- src/com/vinacredit/activity/InformationAccount/InformationAccount_Activity.java
+++ src/com/vinacredit/activity/InformationAccount/InformationAccount_Activity.java
@@ -3,21 +3,24 @@ package com.vinacredit.activity.InformationAccount;
 import com.vinacredit.activity.R;
 import com.vinacredit.activity.Account.Account_Activity;
 import com.vinacredit.activity.Sale.Sale_Activity;
+import com.vinacredit.Resource.*;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 
 public class InformationAccount_Activity extends Activity {
 
-	private EditText edtFirstname, edtLastname, edtCompany, edtAddressl, edtEmail
-			, edtOldPass, edtNewpass, edtConfirmPass;
-	private Button btnContinue;
+	private EditText 	edtFirstname, edtLastname, edtCompany, edtAddressl, edtEmail
+						, edtOldPass, edtNewpass, edtConfirmPass;
+	private Button 		btnContinue;
 	private ImageButton imgUsername;
+	private TextView	txtTitleBar, txtEmail, txtOldPass, txtNewPass, txtConfirmPass;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +38,13 @@ public class InformationAccount_Activity extends Activity {
 		edtConfirmPass 	= (EditText)findViewById(R.id.edtConfirmPass);
 		btnContinue    	= (Button)findViewById(R.id.btnContinue);
 		imgUsername		= (ImageButton)findViewById(R.id.imgUsername);
+		txtTitleBar		= (TextView)findViewById(R.id.txtTitleBar);
+		txtEmail		= (TextView)findViewById(R.id.txtEmail);
+		txtOldPass		= (TextView)findViewById(R.id.txtOldPass);
+		txtNewPass		= (TextView)findViewById(R.id.txtNewPass);
+		txtConfirmPass	= (TextView)findViewById(R.id.txtConfirmPass);
+		
+		translate();
 		
 		//action button continue
 		btnContinue.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +56,22 @@ public class InformationAccount_Activity extends Activity {
 				startActivity(i);
 			}
 		});
+	}
+	private void translate() {
+		// TODO Auto-generated method stub
+		edtFirstname.setHint(MACROS.INFOR_FIRSTNAME_TXT);
+		edtLastname.setHint(MACROS.INFOR_LASTNAME_TXT);
+		edtCompany.setHint(MACROS.INFOR_COMPANY_TXT);
+		edtOldPass.setHint(MACROS.INFOR_OLDPASS_TXT);
+		edtNewpass.setHint(MACROS.INFOR_NEWPASS_TXT);
+		edtConfirmPass.setHint(MACROS.INFOR_CONFIRMPASS_TXT);
+		btnContinue.setHint(MACROS.INFOR_CONTINUE_BTN);
+		
+		txtEmail.setText(MACROS.INFOR_EMAIL_LBL);
+		txtOldPass.setText(MACROS.INFOR_OLDPASS_LBL);
+		txtNewPass.setText(MACROS.INFOR_NEWPASS_LBL);
+		txtConfirmPass.setText(MACROS.INFOR_CONFIRMPASS_LBL);
+		txtTitleBar.setText(MACROS.INFOR_LBL);
 	}
 
 }
