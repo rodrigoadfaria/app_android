a48cc0de54fc15d425661a8b896d78429fda47c0
Subasa1985 <nguyen.luphuc@gmail.com>
2012-11-07 15:18:26 +0700

    + add MACROS.java

---
+++ src/com/vinacredit/Resource/MACROS.java
@@ -0,0 +1,118 @@
+package com.vinacredit.Resource;
+
+public class MACROS{
+    
+    // variable 
+    public static  boolean     bl_language  	= true; // true is english and false is vietnam
+    public static  boolean     tax_status_bl 	= false;// false = off, true = on	
+    
+    //=====WELCOME==============
+    public static  String      WELCOME_TO_VINACREDIT_LBL;// lbl : label
+    public static  String      WELCOME_TEXT_LBL;
+    public static  String      WELCOME_SIGNIN_BTN;// btn : button 
+
+    //=====SIGN IN==============
+    public static  String      SIGNIN_LBL;
+    public static  String      SIGNIN_CANCEL_BTN;
+    public static  String      SIGNIN_BTN;
+    public static  String      SIGNIN_FORGOT_BTN;
+    public static  String      SIGNIN_STATUS_LBL;
+    public static  String      SIGNIN_EMAIL_TXT; // TXT : Textfield
+    public static  String      SIGNIN_PASS_TXT;
+    public static  String      SIGNIN_RESULT_LOGIN_TRUE_LBL;
+    public static  String      SIGNIN_RESULT_LOGIN_FALSE_LBL;
+
+    //=====WRONG PASS===========
+    public static  String      WRONGPASS_LBL;
+    public static  String      WRONGPASS_EMAIL_TXT;
+    public static  String      WRONGPASS_SEND_BTN;
+    public static  String	   WRONGPASS_CANCEL_BTN;
+    public static  String      WRONGPASS_RESULT_LBL;
+    
+
+    //=====INFOR================
+    public static  String      INFOR_LBL;
+    public static  String      INFOR_ENTER_LBL;
+    public static  String      INFOR_CONTINUE_BTN;
+    public static  String      INFOR_FIRSTNAME_TXT;
+    public static  String      INFOR_LASTNAME_TXT;
+    public static  String      INFOR_COMPANY_TXT;
+    public static  String      INFOR_ADDRESS_TXT;
+    public static  String      INFOR_OLDPASS_TXT;
+    public static  String      INFOR_NEWPASS_TXT;
+    public static  String      INFOR_CONFIRMPASS_TXT;
+    public static  String      INFOR_EMAIL_LBL;
+    public static  String      INFOR_OLDPASS_LBL;
+    public static  String      INFOR_NEWPASS_LBL;
+    public static  String      INFOR_CONFIRMPASS_LBL;
+
+    //=====SALE================
+    public static  String      SALE_LBL;
+    public static  String      SALE_TAKEPHOTO_LBL;
+    public static  String      SALE_CHOOSEPHOTO_LBL;
+    public static  String      SALE_DELETEPHOTO_LBL;
+    public static  String      SALE_CANCEL_LBL;
+    public static  String      SALE_SWIPE_CARD;
+
+    //=====CHARGE==============
+    public static  String      CHARGE_LBL;
+    public static  String      CHARGE_CASH_LBL;
+    public static  String      CHARGE_TENDER_BTN;
+
+    //=====RECEIPT==============
+    public static  String      RECEIPT_EMAIL_TXT;
+    public static  String      RECEIPT_SKIP_BTN;
+    public static  String      RECEIPT_SEND_BTN;
+    public static  String      RECEIPT_RESULT_TRUE_LBL;
+    public static  String      RECEIPT_RESULT_FALSE_LBL;
+
+    //=====IDENTIFY=============
+    public static  String      IDENTIFY_LBL;
+    public static  String      IDENTIFY_RIGHT_BTN;
+    public static  String      IDENTIFY_TEXT_LBL;
+    public static  String      IDENTIFY_PIC_LBL;
+    public static  String      IDENTIFY_TAKEPHOTO_BTN;
+
+    //=====SIGNATURE============
+    public static  String      SIGNATURE_LBL;
+    public static  String      SIGNATURE_RIGHT_BTN;
+    public static  String      SIGNTURE_CLEAR_BTN;
+    public static  String      SIGNATURE_TEXT_LBL;
+
+    //=====SENDING==============
+    public static  String      SEND_LBL;
+
+    //=====ACCOUNT==============
+    public static  String      ACCOUNT_LBL;
+    public static  String      ACCOUNT_RIGHT_BTN;
+    public static  String      ACCOUNT_SALEHISTORY_LBL;
+    public static  String      ACCOUNT_TAX_LBL;
+    public static  String      ACCOUNT_SUPPORT_LBL;
+    public static  String      ACCOUNT_CANCEL_LBL;
+
+    //=====SALE HISTORY=========
+    public static  String      SALEHISTORY_LBL;
+
+    //=====DETAIL SALE==========
+    public static  String      DETAILSALE_LBL;
+    public static  String      DETAILSALE_TOTAL_LBL;
+    public static  String      DETAILSALE_BILL_LBL;
+
+    //=====TAX=================
+    public static  String      TAX_LBL;
+    public static  String      TAX_SALETAX_LBL;
+    public static  String      TAX_TAXRATE_LBL;
+    public static  String      TAX_TEXT_LBL;
+    
+    //=====SUPPORT=============
+    public static  String      SUPPORT_LBL;
+    public static  String      SUPPORT_VINA_LBL;
+    public static  String      SUPPORT_HOTLINE_LBL;
+    public static  String      SUPPORT_INSTRU_LBL;
+
+    //=====DONE================
+    public static  String      DONE_PAID_LBL;
+    public static  String      DONE_BTN;
+
+    
+}
