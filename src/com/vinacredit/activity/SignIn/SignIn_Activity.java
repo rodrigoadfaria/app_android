df8776fcec22934e9663c1311d636a4e1704214b
viethung232@gmail.com <viethung232@gmail.com>
2012-11-12 13:27:26 +0700

    -Hung modify SignIn_Activity, MySQLiteHelper

--- src/com/vinacredit/activity/SignIn/SignIn_Activity.java
+++ src/com/vinacredit/activity/SignIn/SignIn_Activity.java
@@ -1,9 +1,16 @@
 package com.vinacredit.activity.SignIn;
 
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 import com.vinacredit.Resource.*;
 import com.vinacredit.activity.R;
 import com.vinacredit.activity.InformationAccount.InformationAccount_Activity;
 import com.vinacredit.activity.SignIn.WrongPass.WrongPass_Activity;
+import com.vinacredit.database.MySQLiteHelper;
+
+import con.vinacredit.DTO.Account;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,12 +19,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignIn_Activity extends Activity{
 
 	private EditText 	edtUsername, edtPassword;
 	private Button 		btnSignIn, btnWrongPass;
 	private TextView	txtTitleBar;
+	private MySQLiteHelper dbaccount;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -33,14 +42,30 @@ public class SignIn_Activity extends Activity{
 		btnWrongPass 	= (Button)findViewById(R.id.btnWrongPass);
 		txtTitleBar		= (TextView)findViewById(R.id.txtTitleBar);
 		translate();
-		
+		dbaccount = new MySQLiteHelper(this);
+		final Account account = new Account();
+		account.setEmail("hung");
+		account.setPass("123");
+		dbaccount.AddAccount(account);
 		btnSignIn.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				try{
+				if(dbaccount.Login(edtUsername.getText().toString(), edtPassword.getText().toString())){
 				Intent intent = new Intent(getApplicationContext(),InformationAccount_Activity.class);
 				startActivity(intent);
+					Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_LONG).show();
+				} else {
+					Toast.makeText(getApplicationContext(), "Login do not successful", Toast.LENGTH_LONG).show();
+				}
+				
+				} catch (Exception e) {
+					// TODO: handle exception
+					Toast.makeText(SignIn_Activity.this,e.getMessage(), Toast.LENGTH_LONG).show();
+				}
+				dbaccount.close();
 			}
 		});
 		
---
+++ src/com/vinacredit/database/MySQLiteHelper.java
@@ -0,0 +1,144 @@
+
+package com.vinacredit.database;
+
+import con.vinacredit.DTO.Account;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLiteHelper 
+{ 
+    
+    private static final String DATABASE_NAME = "database";
+    private static final String TAG = "DBAdapter";
+    private static final int DATABASE_VERSION = 1;
+    
+    /*----- Set name table database -----*/
+    private static final String DATABASE_TABLE_ACCOUNT = "Account";
+    private static final String DATABASE_TABLE_BILL = "Bill";
+    private static final String DATABASE_TABLE_SUMBILL = "SumBill";
+    
+    /*----- Set key name in table database ACCOUNT -----*/
+    public static final String KEY_EMAIL_ACCOUNT	= "email";
+    public static final String KEY_PASSWORD 		= "password";
+    public static final String KEY_FIRSTNAME 		= "firstname";
+    public static final String KEY_LASTNAME			= "lastname";
+    public static final String KEY_COMPANY 			= "company";
+    public static final String KEY_ADDRESS 			= "address";
+    public static final String KEY_IMAGE			= "image";
+    
+    /*----- Set key name in table database BILL -----*/
+    public static final String KEY_TIMESALE			= "timeSale";
+    public static final String KEY_SUMITEM 			= "sumItem";
+    public static final String KEY_EMAIL_BILL 		= "email";
+    public static final String KEY_DATESALE_BILL	= "dateSale";
+    
+    /*----- Set key name in table database SUMBILL -----*/
+    public static final String KEY_DATESALE			= "dateSale";
+    public static final String KEY_SUMBILL_SUMBILL	= "sumBill";
+    public static final String KEY_EMAIL_SUMBILL 	= "email";
+    
+
+    private static final String DATABASE_CREATE_ACCOUNT =
+        "create table Account ("
+        + "email text primary key not null," 
+        + "password text not null,"
+        + "firstname text,"
+        + "lastname text,"
+        + "company text,"
+        + "address text,";
+    
+    private static final String DATABASE_CREATE_BILL = 
+    	"create table Bill ("
+    	+ "timeSale text,"
+    	+ "sumItem text,"
+    	+ "email text,"
+    	+ "dateSale text);";
+    
+    private static final String DATABASE_CREATE_SUMBILL = 
+    	"create table SumBill ( "
+    	+ "dateSale text,"
+    	+ "sumBill text,"
+    	+ "email text);";
+    
+    
+    
+    
+    private Context context = null;  
+    private DatabaseHelper DBHelper;
+    private SQLiteDatabase db;
+
+    public MySQLiteHelper(Context ctx) 
+    {
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+        
+    private static class DatabaseHelper extends SQLiteOpenHelper 
+    {
+        DatabaseHelper(Context context) 
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) 
+        {
+            db.execSQL(DATABASE_CREATE_ACCOUNT);
+            db.execSQL(DATABASE_CREATE_BILL);
+            db.execSQL(DATABASE_CREATE_SUMBILL);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) 
+        {
+            Log.w(TAG, "Upgrading database from version " + oldVersion 
+                    + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS Account;");
+            db.execSQL("DROP TABLE IF EXISTS Bill;");
+            db.execSQL("DROP TABLE IF EXISTS SumBill;");
+            onCreate(db);
+        }
+    }    
+    
+    
+    public void open() throws SQLException 
+    {
+        db = DBHelper.getWritableDatabase();
+    }
+
+      
+    public void close() 
+    {
+        DBHelper.close();
+    }    
+    
+    public long AddAccount(Account account) throws SQLException 
+    {
+    	open();
+    	ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_EMAIL_ACCOUNT, account.getEmail());
+        initialValues.put(KEY_PASSWORD, account.getPass());
+        return db.insert(DATABASE_TABLE_ACCOUNT, null, initialValues);
+    }
+
+    public boolean Login(String email, String password) throws SQLException 
+    {
+    	open();
+    	Cursor mCursor = db.rawQuery("SELECT * FROM " + DATABASE_TABLE_ACCOUNT + " WHERE email=? AND password=?", new String[]{email,password});
+        if (mCursor != null) {           
+            if(mCursor.getCount() > 0)
+            {
+            	return true;
+            }
+        }
+        mCursor.close();
+     return false;
+    }
+
+}
