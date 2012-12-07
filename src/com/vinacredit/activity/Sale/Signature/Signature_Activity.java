c15d48f335d5642c123aee7db1634889a302eaf7
viethung232@gmail.com <viethung232@gmail.com>
2012-12-07 14:03:45 +0700

    -hung modify Signature

--- src/com/vinacredit/activity/Sale/Signature/Signature_Activity.java
+++ src/com/vinacredit/activity/Sale/Signature/Signature_Activity.java
@@ -1,5 +1,9 @@
 package com.vinacredit.activity.Sale.Signature;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
 import com.vinacredit.Resource.MACROS;
 import com.vinacredit.activity.R;
 import com.vinacredit.activity.Sale.Charge.Charge_Activity;
@@ -7,27 +11,52 @@ import com.vinacredit.activity.Sale.Identify.Identify_Activity;
 import com.vinacredit.activity.Sale.Sending.Sending_Activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore.Images;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Signature_Activity extends Activity{
 
 	private TextView	txtTitleBar, txtPrice, txtName;
 	private Button		btnIdentify, btnSending, btnClear;
-	private DrawView	drawView;
+	LinearLayout mContent;
+    signature mSignature;
+    public static String tempDir;
+    public int count = 1;
+    public String current = null;
+    private Bitmap mBitmap;
+    View mView;
+    File mypath;
+ 
+    private String uniqueId;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signature);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		drawView		= (DrawView)findViewById(R.id.drawing_screen_drawview);
-		drawView.requestFocus();
 		initialize();
 	}
 
@@ -43,6 +72,23 @@ public class Signature_Activity extends Activity{
 		SharedPreferences s = this.getSharedPreferences("EMAIL", MODE_WORLD_READABLE);
 		txtPrice.setText(s.getString("SUMPRICE", "0")+ " VND.");
 		translate();
+		
+		
+		/* Draw signature */
+		tempDir = Environment.getExternalStorageDirectory() + "/" + getResources().getString(R.string.external_dir) + "/";
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir(getResources().getString(R.string.external_dir), Context.MODE_PRIVATE);
+ 
+        prepareDirectory();
+        uniqueId = getTodaysDate() + "_" + getCurrentTime() + "_" + Math.random();
+        current = uniqueId + ".png";
+        mypath= new File(directory,current);
+ 
+ 
+        mContent = (LinearLayout) findViewById(R.id.linearLayout);
+        mSignature = new signature(this, null);
+        mSignature.setBackgroundColor(Color.WHITE);
+        mContent.addView(mSignature, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 	}
 	//action btnIdentify
 	public void gotoIdentify(View view){
@@ -57,7 +103,7 @@ public class Signature_Activity extends Activity{
 	}
 	
 	public void Clear(View v){
-		
+		mSignature.clear();
 	}
 	
 	private void translate() {
@@ -70,5 +116,212 @@ public class Signature_Activity extends Activity{
 		// TODO Auto-generated method stub
 //		super.onBackPressed();
 	}
+	
+	
+	
+    private String getTodaysDate() { 
+    	 
+        final Calendar c = Calendar.getInstance();
+        int todaysDate =     (c.get(Calendar.YEAR) * 10000) + 
+        ((c.get(Calendar.MONTH) + 1) * 100) + 
+        (c.get(Calendar.DAY_OF_MONTH));
+        Log.w("DATE:",String.valueOf(todaysDate));
+        return(String.valueOf(todaysDate));
+ 
+    }
+ 
+    private String getCurrentTime() {
+ 
+        final Calendar c = Calendar.getInstance();
+        int currentTime =     (c.get(Calendar.HOUR_OF_DAY) * 10000) + 
+        (c.get(Calendar.MINUTE) * 100) + 
+        (c.get(Calendar.SECOND));
+        Log.w("TIME:",String.valueOf(currentTime));
+        return(String.valueOf(currentTime));
+ 
+    }
+	
+	private boolean prepareDirectory() 
+    {
+        try
+        {
+            if (makedirs()) 
+            {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+            Toast.makeText(this, "Could not initiate File System.. Is Sdcard mounted properly?", 1000).show();
+            return false;
+        }
+    }
+ 
+    private boolean makedirs() 
+    {
+        File tempdir = new File(tempDir);
+        if (!tempdir.exists())
+            tempdir.mkdirs();
+ 
+        if (tempdir.isDirectory()) 
+        {
+            File[] files = tempdir.listFiles();
+            for (File file : files) 
+            {
+                if (!file.delete()) 
+                {
+                    System.out.println("Failed to delete " + file);
+                }
+            }
+        }
+        return (tempdir.isDirectory());
+    }
+	
+	/* class signature */
+    public class signature extends View 
+    {
+        private static final float STROKE_WIDTH = 3f;
+        private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
+        private Paint paint = new Paint();
+        private Path path = new Path();
+ 
+        private float lastTouchX;
+        private float lastTouchY;
+        private final RectF dirtyRect = new RectF();
+ 
+        public signature(Context context, AttributeSet attrs) 
+        {
+            super(context, attrs);
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeWidth(STROKE_WIDTH);
+        }
+ 
+        public void save(View v) 
+        {
+            Log.v("log_tag", "Width: " + v.getWidth());
+            Log.v("log_tag", "Height: " + v.getHeight());
+            if(mBitmap == null)
+            {
+                mBitmap =  Bitmap.createBitmap (mContent.getWidth(), mContent.getHeight(), Bitmap.Config.RGB_565);;
+            }
+            Canvas canvas = new Canvas(mBitmap);
+            try
+            {
+                FileOutputStream mFileOutStream = new FileOutputStream(mypath);
+ 
+                v.draw(canvas); 
+                mBitmap.compress(Bitmap.CompressFormat.PNG, 90, mFileOutStream); 
+                mFileOutStream.flush();
+                mFileOutStream.close();
+                String url = Images.Media.insertImage(getContentResolver(), mBitmap, "title", null);
+                Log.v("log_tag","url: " + url);
+                //In case you want to delete the file
+                //boolean deleted = mypath.delete();
+                //Log.v("log_tag","deleted: " + mypath.toString() + deleted);
+                //If you want to convert the image to string use base64 converter
+ 
+            }
+            catch(Exception e) 
+            { 
+                Log.v("log_tag", e.toString()); 
+            } 
+        }
+ 
+        public void clear() 
+        {
+            path.reset();
+            invalidate();
+        }
+ 
+        @Override
+        protected void onDraw(Canvas canvas) 
+        {
+            canvas.drawPath(path, paint);
+        }
+ 
+        @Override
+        public boolean onTouchEvent(MotionEvent event) 
+        {
+            float eventX = event.getX();
+            float eventY = event.getY();
+            btnSending.setEnabled(true);
+ 
+            switch (event.getAction()) 
+            {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                lastTouchX = eventX;
+                lastTouchY = eventY;
+                return true;
+ 
+            case MotionEvent.ACTION_MOVE:
+ 
+            case MotionEvent.ACTION_UP:
+ 
+                resetDirtyRect(eventX, eventY);
+                int historySize = event.getHistorySize();
+                for (int i = 0; i < historySize; i++) 
+                {
+                    float historicalX = event.getHistoricalX(i);
+                    float historicalY = event.getHistoricalY(i);
+                    expandDirtyRect(historicalX, historicalY);
+                    path.lineTo(historicalX, historicalY);
+                }
+                path.lineTo(eventX, eventY);
+                break;
+ 
+            default:
+                debug("Ignored touch event: " + event.toString());
+                return false;
+            }
+ 
+            invalidate((int) (dirtyRect.left - HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.top - HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.right + HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+ 
+            lastTouchX = eventX;
+            lastTouchY = eventY;
+ 
+            return true;
+        }
+ 
+        private void debug(String string){
+        }
+ 
+        private void expandDirtyRect(float historicalX, float historicalY) 
+        {
+            if (historicalX < dirtyRect.left) 
+            {
+                dirtyRect.left = historicalX;
+            } 
+            else if (historicalX > dirtyRect.right) 
+            {
+                dirtyRect.right = historicalX;
+            }
+ 
+            if (historicalY < dirtyRect.top) 
+            {
+                dirtyRect.top = historicalY;
+            } 
+            else if (historicalY > dirtyRect.bottom) 
+            {
+                dirtyRect.bottom = historicalY;
+            }
+        }
+ 
+        private void resetDirtyRect(float eventX, float eventY) 
+        {
+            dirtyRect.left = Math.min(lastTouchX, eventX);
+            dirtyRect.right = Math.max(lastTouchX, eventX);
+            dirtyRect.top = Math.min(lastTouchY, eventY);
+            dirtyRect.bottom = Math.max(lastTouchY, eventY);
+        }
+    }
 
 }
